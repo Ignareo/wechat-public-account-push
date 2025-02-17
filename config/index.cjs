4ac5bb88e36abb95578bc932e743fb7c403e488e
Chtholly.Ruq.Seniorious <56428448+Ignareo@users.noreply.github.com>
2025-02-17 23:52:31 +0800

    Update index.cjs 新模板

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ohK9R7PqVHDkIjtWm3uBlo99XKw4',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'BMux5Wsps7ZyNHX2zkNE6m3Q7q0eGEoBmT2fxH9wBTs',
+      useTemplateId: '3ArIJHhY8mYrCBfVEuyoJJA5DCpmx3-m8xt0JLFnU2c',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '09-13',
       festivals: [
