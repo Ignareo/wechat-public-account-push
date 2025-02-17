897f6e115865c23dcb36aa1a03873e9437bf3229
Chtholly.Ruq.Seniorious <56428448+Ignareo@users.noreply.github.com>
2025-02-17 18:44:11 +0800

    Update index.cjs 使用模板

--- config/index.cjs
+++ config/index.cjs
@@ -1,42 +1,61 @@
-{
-  "APP_ID": 'wx04da0eea8f0cfe8c',
-  "APP_SECRET": 'e92ed058e8d6a3867f494bf0dfc332d7',
-  "IS_SHOW_COLOR": true,
-  "CALLBACK_TEMPLATE_ID": "pGlOSdSNiFxMVv9Axh4xLOPQnudtW48PJGVle8TiZC0",
-  "CALLBACK_USERS": [{ "name": "自己", "id": "ohK9R7LvSYtgDFpesJd9c1zd7REs" }],
-  "USERS": [
+/* eslint-disable */
+
+/**
+ * 此项目配置为方便新人使用，已缩减至最简配置。
+ * 如若想使用更多功能，请查考文档中的 【3. config参数说明】 
+ * 自行添加属性，以支持更多个性化功能
+ */
+const USER_CONFIG = {
+
+  // 使用微信测试号：公众号APP_ID
+  APP_ID: 'wx04da0eea8f0cfe8c',
+
+  // 使用微信测试号：公众号APP_SECRET
+  APP_SECRET: 'e92ed058e8d6a3867f494bf0dfc332d7',
+
+  PROVINCE: '福建',
+  CITY: '泉州',
+
+  USERS: [
     {
-      "name": "宝宝~",
-      "id": "ohK9R7PqVHDkIjtWm3uBlo99XKw4",
-      "useTemplateId": "BMux5Wsps7ZyNHX2zkNE6m3Q7q0eGEoBmT2fxH9wBTs",
-      "province": "福建",
-      "city": "泉州",
-      "horoscopeDate": "09-13",
-      "horoscopeDateType": "今日",
-      "openUrl": "https://www.hqu.edu.cn/",
-      "festivals": [
+      // 想要发送的人的名字
+      name: '宝宝~',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'ohK9R7PqVHDkIjtWm3uBlo99XKw4',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: 'BMux5Wsps7ZyNHX2zkNE6m3Q7q0eGEoBmT2fxH9wBTs',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '09-13',
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          "type": "节日",
-          "name": "清明节放假",
-          "date": "04-04",
-          "year": "2025"
-        }
+          type: '*生日', name: '宝贝', year: '2000', date: '08-17',
+        },
+        {
+          type: '节日', name: '第一次聊天', year: '2024', date: '09-27',
+        },
       ],
-      "customizedDateList": [
-        { "keyword": "2025年过去了", "date": "2025-01-01" }
-      ]
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2024-09-27' },
+      ],
+    },
+  ],
+
+
+  // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
+  CALLBACK_TEMPLATE_ID: 'pGlOSdSNiFxMVv9Axh4xLOPQnudtW48PJGVle8TiZC0',
+
+  CALLBACK_USERS: [
+    {
+      name: '自己',
+      // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'ohK9R7LvSYtgDFpesJd9c1zd7REs',
     }
   ],
-  "SWITCH": {
-    "weather": true,
-    "holidaytts": true,
-    "CIBA": true,
-    "oneTalk": false,
-    "earthyLoveWords": false,
-    "momentCopyrighting": false,
-    "poisonChickenSoup": false,
-    "poetry": false,
-    "horoscope": false,
-    "birthdayMessage": true
-  }
+
 }
+
+module.exports = USER_CONFIG
+
